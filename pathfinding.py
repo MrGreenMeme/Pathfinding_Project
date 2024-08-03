858e32dff787f6aa7cc16e266494d8da4685a7fc
MrGreenMeme <lorenzflich@gmail.com>
2024-08-03 14:35:55 +0200

    feat(toolbar): add zoom functionality + adjusted rendering

--- pathfinding.py
+++ pathfinding.py
@@ -12,31 +12,31 @@ class Cube:
 
 
 class Grid:
-    def __init__(self, rows, cols, cube_size):
+    def __init__(self, rows, cols):
         self.rows = rows
         self.cols = cols
-        self.cube_size = cube_size
         self.grid = [[Cube() for _ in range(cols)] for _ in range(rows)]
         self.start_cube = None
         self.goal_cube = None
 
-    def draw_cube(self, screen, x, y, offset_x, offset_y):
+    def draw_cube(self, screen, x, y, cube_size, offset_x, offset_y):
         cube = self.grid[y][x]
         # inside cell
-        pygame.draw.rect(screen, cube.color, (offset_x + x * self.cube_size, offset_y + y * self.cube_size, self.cube_size, self.cube_size))
+        if cube.color != "white":
+            pygame.draw.rect(screen, cube.color, (offset_x + x * cube_size, offset_y + y * cube_size, cube_size, cube_size))
         # outline of cell
-        pygame.draw.rect(screen, "black", (offset_x + x * self.cube_size, offset_y + y * self.cube_size, self.cube_size, self.cube_size), 1)
+        pygame.draw.rect(screen, "black", (offset_x + x * cube_size, offset_y + y * cube_size, cube_size, cube_size), 1)
 
-    def center_grid(self, window_width, window_height):
-        grid_width = self.cols * self.cube_size
-        grid_height = self.rows * self.cube_size
+    def center_grid(self, window_width, window_height, cube_size):
+        grid_width = self.cols * cube_size
+        grid_height = self.rows * cube_size
         offset_x = (window_width - grid_width) // 2
         offset_y = (window_height - grid_height) // 2
         return offset_x, offset_y
 
-    def handle_click(self, x, y, offset_x, offset_y, selected_tool):
-        grid_x = (x - offset_x) // self.cube_size
-        grid_y = (y - offset_y) // self.cube_size
+    def handle_click(self, x, y, cube_size, offset_x, offset_y, selected_tool):
+        grid_x = (x - offset_x) // cube_size
+        grid_y = (y - offset_y) // cube_size
         print(f"x: {x} y: {y} grid_x: {grid_x} grid_y: {grid_y} selected_tool: {selected_tool}")
         if 0 <= grid_x < self.cols and 0 <= grid_y < self.rows:
             cube = self.grid[grid_y][grid_x]
@@ -60,7 +60,6 @@ class Grid:
         data = {
             "rows": self.rows,
             "cols": self.cols,
-            "cube_size": self.cube_size,
             "grid": [[{"color": cube.color, "traversable": cube.traversable} for cube in row] for row in self.grid],
             "start_cube": self.start_cube,
             "goal_cube": self.goal_cube,
@@ -74,7 +73,6 @@ class Grid:
             data = json.load(f)
         self.rows = data["rows"]
         self.cols = data["cols"]
-        self.cube_size = data["cube_size"]
         self.grid = [[Cube(**cube_data) for cube_data in row] for row in data["grid"]]
         self.start_cube = data["start_cube"]
         self.goal_cube = data["goal_cube"]
@@ -101,32 +99,38 @@ class Toolbar:
         if y < self.tool_size and x < (self.tool_size * (len(self.tools) + 1)):
             self.selected_tool = x // (self.tool_size + self.tool_spacing)
 
+def load_image(file_path, size):
+    image = pygame.image.load(file_path)
+    image = pygame.transform.scale(image, (size, size))
+    return image.convert()
+
 # pygame setup
 pygame.init()
-
-# grid setup
-rows, cols = (15, 15)
-cube_size = 50
-grid = Grid(rows, cols, cube_size)
-
-# toolbar setup
-flag_img = pygame.image.load('flag.png')
-flag_img = pygame.transform.scale(flag_img, (cube_size, cube_size))
-play_img = pygame.image.load('play.png')
-play_img = pygame.transform.scale(play_img, (cube_size, cube_size))
-save_img = pygame.image.load('save.png')
-save_img = pygame.transform.scale(save_img, (cube_size, cube_size))
-load_img = pygame.image.load('load.png')
-load_img = pygame.transform.scale(load_img, (cube_size, cube_size))
-toolbar = Toolbar([("start", "green"), ("goal", flag_img), ("obstacle", "grey"), ("play", play_img), ("save", save_img), ("load", load_img)], 50, 10)
 
 # window setup
 window_width, window_height = 1000, 1000
 screen = pygame.display.set_mode((window_width, window_height), pygame.RESIZABLE)
 pygame.display.set_caption("Pathfinding")
 
+# grid setup
+rows, cols = (20, 20)
+grid = Grid(rows, cols)
+cube_size = 40
+
+# toolbar setup
+tool_size = 50
+flag_img = load_image('flag.png', tool_size)
+play_img = load_image('play.png', tool_size)
+save_img = load_image('save.png', tool_size)
+load_img = load_image('load.png', tool_size)
+toolbar = Toolbar([("start", "green"), ("goal", flag_img), ("obstacle", "grey"), ("play", play_img), ("save", save_img), ("load", load_img)], 50, 10)
+
+# zoom
+zoom_factor = 1.0
+zoom_increment = 0.1
+
 # center grid
-center_x, center_y = grid.center_grid(window_width, window_height)
+center_x, center_y = grid.center_grid(window_width, window_height, int(cube_size * zoom_factor))
 
 # Game loop
 clock = pygame.time.Clock()
@@ -144,7 +148,7 @@ while running:
             print("x: " +  str(new_window_width) + " y: " + str(new_window_height))
 
             screen = pygame.display.set_mode((new_window_width, new_window_height), pygame.RESIZABLE)
-            center_x, center_y = grid.center_grid(new_window_width, new_window_height)
+            center_x, center_y = grid.center_grid(new_window_width, new_window_height, int(cube_size * zoom_factor))
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1: # only accept left-click
@@ -161,9 +165,17 @@ while running:
                     filename = filedialog.askopenfilename(initialdir=os.getcwd(), filetypes=[("JSON files", "*.json")])
                     if filename:
                         grid.load_grid(filename)
-                        center_x, center_y = grid.center_grid(window_width, window_height)
+                        center_x, center_y = grid.center_grid(window_width, window_height, int(cube_size * zoom_factor))
                 else:
-                    grid.handle_click(x,y, center_x, center_y, toolbar.selected_tool)
+                    grid.handle_click(x,y, int(cube_size * zoom_factor), center_x, center_y, toolbar.selected_tool)
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                zoom_factor += zoom_increment
+                center_x, center_y = grid.center_grid(window_width, window_height, int(cube_size * zoom_factor))
+            elif event.key == pygame.K_DOWN:
+                zoom_factor = max(zoom_increment, zoom_factor - zoom_increment)
+                center_x, center_y = grid.center_grid(window_width, window_height, int(cube_size * zoom_factor))
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
@@ -174,7 +186,7 @@ while running:
     start_time = time.perf_counter()
     for y in range(grid.rows):
         for x in range(grid.cols):
-            grid.draw_cube(screen, x, y, center_x, center_y)
+            grid.draw_cube(screen, x, y, int(cube_size * zoom_factor), center_x, center_y)
     end_time = time.perf_counter()
     elapsed_time = (end_time - start_time) * 1000  # Convert to milliseconds
     print(f"Function executed in {elapsed_time:.2f} ms")
